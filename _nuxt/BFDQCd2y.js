1a6d147818b5d4ee5493d7cfd6dc46356686161f
itisAliRH <itisAliRH@users.noreply.github.com>
2025-06-23 14:25:57 +0000

    deploy: 2ba251539071138c3338a65f0c38da0f96d7c347

---
+++ _nuxt/BFDQCd2y.js
@@ -0,0 +1,1 @@
+import{aK as e,cV as s}from"./CAWAI8W9.js";const c={__name:"[search]",setup(r){const a=e().params.search||"";return s(`/?search=${a}`),()=>{}}};export{c as default};
--- _nuxt/CWYnXuLx.js
+++
@@ -1,1 +0,0 @@
-import{aK as e,cV as s}from"./EclRxj0G.js";const c={__name:"[search]",setup(r){const a=e().params.search||"";return s(`/?search=${a}`),()=>{}}};export{c as default};
